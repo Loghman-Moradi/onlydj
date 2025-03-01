a3c8c6d85e1363792ffea7a2845baaf20214b08d
Loghman <loghman7979@gmail.com>
2025-03-01 10:24:56 +0330

    Update shop.py

--- shop.py
+++ shop.py
@@ -42,7 +42,48 @@ age = 18
 city = "L.A"
 
 
+def login():
+    return False
 
+
+def logout():
+    return True
+
+
+def register():
+    username = ""
+    password_1 = ""
+    password_2 = ""
+    code = ""
+
+
+def Change_password():
+    return True
+
+
+def test():
+    des = 'te1224'
+    return des
+
+
+def like_post():
+    return test()
+
+
+class Profile():
+    def __init__(self, name):
+        return self.name
+
+
+def edit(user):
+    return user
+
+
+# this is a test in github
+phone = "123456789"
+name = "via"
+age = 18
+city = "L.A"
 
 
 
